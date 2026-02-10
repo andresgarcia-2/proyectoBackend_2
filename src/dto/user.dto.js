5dc18c15b564424d771574e0ca9e8c4201f7fb70
andres garcia <andresagustingp@gmail.com>
2026-02-10 16:14:26 -0300

    datos de usuario

---
+++ src/dto/user.dto.js
@@ -0,0 +1,23 @@
+export class UserDTO {
+    constructor(user) {
+        this.id = user._id;
+        this.first_name = user.first_name;
+        this.last_name = user.last_name;
+        this.full_name = `${user.first_name} ${user.last_name}`;
+        this.email = user.email;
+        this.age = user.age;
+        this.role = user.role;
+        this.cart = user.cart;
+        this.createdAt = user.createdAt;
+        this.lastLogin = user.lastLogin;
+    }
+}
+
+export class UserTokenDTO {
+    constructor(user) {
+        this.id = user._id;
+        this.email = user.email;
+        this.role = user.role;
+        this.cart = user.cart;
+    }
+}
