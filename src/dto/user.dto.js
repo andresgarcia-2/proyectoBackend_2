a1b6438aeba1db85b4e0d99e9819fc016fb0c1eb
andres garcia <andresagustingp@gmail.com>
2026-03-12 21:55:10 -0300

    actualize user.dto.js

--- src/dto/user.dto.js
+++ src/dto/user.dto.js
@@ -28,3 +28,34 @@ export class UserRegisterDTO {
         this.password = data.password;
     }
 }
+
+export class UserDTO {
+    constructor(user) {
+        this.id = user._id;
+        this.email = user.email;
+        this.firstName = user.firstName;
+        this.lastName = user.lastName;
+        this.role = user.role;
+        this.cart = user.cart;
+        this.createdAt = user.createdAt;
+    }
+}
+
+export class UserPublicDTO {
+    constructor(user) {
+        this.id = user._id;
+        this.email = user.email;
+        this.firstName = user.firstName;
+        this.lastName = user.lastName;
+        this.role = user.role;
+    }
+}
+
+export class UserRegisterDTO {
+    constructor(data) {
+        this.email = data.email;
+        this.firstName = data.firstName;
+        this.lastName = data.lastName;
+        this.password = data.password;
+    }
+}
